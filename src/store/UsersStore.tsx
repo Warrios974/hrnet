f0460ea1bd6e7f39aa3e4f2c8c2110a0669e8fea
Kenny Lauret <kenny.lauret@outlook.fr>
2023-10-13 13:36:49 +0400

    Création store avec zustang

---
+++ src/store/UsersStore.tsx
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+export type User = {
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    startDate: string,
+    streetAddress: string,
+    cityAddress: string,
+    stateAddress: string,
+    zipCodeAddress: string,
+    departement: string,
+}
+
+type Store = {
+  users: User[] | []
+  addUser: (user: User) => void
+}
+
+export const useEmployeeStore = create<Store>()((set) => ({
+    users: [],
+    addUser: (newUser: User) => set((state) => ({ users: [...state.users, newUser] })),
+}))
