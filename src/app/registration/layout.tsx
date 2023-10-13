041ceb7d3b5f37ffb8b0cb964485eb03ad3c5006
Kenny Lauret <kenny.lauret@outlook.fr>
2023-10-13 13:35:49 +0400

    Page registration mise en forme

---
+++ src/app/registration/layout.tsx
@@ -0,0 +1,18 @@
+export default function RegistrationLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+        <>
+            <header className="bg-white shadow">
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Registration</h1>
+                </div>
+            </header>
+            <main>
+                {children}
+            </main>
+        </>
+    )
+  }
---
+++ src/app/registration/page.tsx
@@ -0,0 +1,16 @@
+import RegistrationForm from "@/components/form/RegistrationForm"
+
+export default function Page({
+    params,
+    searchParams,
+  }: {
+    params: { slug: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+  }) {
+    return (
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <RegistrationForm />
+        </div>
+            
+    )
+  }
