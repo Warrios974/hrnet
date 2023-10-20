475cf5d9bb66e9843e2d63dbe9fc0ba5e4394f3b
Kenny Lauret <kenny.lauret@outlook.fr>
2023-10-20 14:32:17 +0400

    Mise en place de la page employees

---
+++ src/app/employees/page.tsx
@@ -0,0 +1,13 @@
+export default function Page({
+    params,
+    searchParams,
+  }: {
+    params: { slug: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+  }) {
+    return (
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            
+        </div>
+    )
+  }
